f4e6e425385ec75e763b99b213947b7ec2ede8fa
JESSICA <jessicalookenott15@gmail.com>
2023-07-31 15:40:37 -0400

    First react app

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+// ignore the above for now, we will get to them later
+import product, {numbers, sum} from './math.js' // combine all exports into a math object
+// ! we can name the combined object however we like, it doesn't have to be math
+console.log(numbers.reduce(product));
+console.log(product(10, 5));
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
