a7ae58e53039441a42e84f50a3166be6ca5f9d80
JESSICA <jessicalookenott15@gmail.com>
2023-08-02 10:42:14 -0400

    Added Switch

--- src/App.js
+++ src/App.js
@@ -1,31 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
 import React from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import './App.css';
 
-// ignore the above for now, we will get to them later
-import product, {numbers, sum} from './math.js' // combine all exports into a math object
-// ! we can name the combined object however we like, it doesn't have to be math
-console.log(numbers.reduce(product));
-console.log(product(10, 5));
+const Home = () => {
+  return <h2>Home</h2>;
+}
 
-function App() {
+const About = () => {
+  return <h2>About</h2>;
+}
+
+const Contact = () => {
+  return <h2>Contact</h2>;
+}
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container">
+        <h2>React Rotuer Demo</h2>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about/">About</Link>
+            </li>
+            <li>
+              <a href="/about/">About (normal anchor link, page will full reload)</a>
+            </li>
+            <li>
+              <Link to="/contact/">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+        <Route path="/" component={Home} />
+        <Route path="/about/" component={About} />
+        <Route path="/contact/" component={Contact} />
+        <hr />
+        <p>Using Switch</p>
+        <Switch>
+          <Route path="/" component={Home} />
+          <Route path="/about/" component={About} />
+          <Route path="/contact/" component={Contact} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
